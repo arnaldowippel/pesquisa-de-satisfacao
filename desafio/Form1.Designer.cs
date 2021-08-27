4f7693ca68f907e039cc1dfe0e85fe4552a225eb
Arnaldo Wippel <arnaldowippel@hotmail.com>
2021-08-26 23:23:54 -0300

    Adicionar arquivos de projeto.

---
+++ desafio/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace desafio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "0";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Você ficou satisfeito com o atendimento da nossa loja ?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sim";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Não";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(351, 298);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Satisfação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ desafio/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio
+{
+    public partial class Form1 : Form
+    {
+
+        int contadorSim = 0;
+        int contadorNao = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensagem = "Você deseja confirmar seu voto ? ";
+            string title = "Confirmação";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(mensagem, title, buttons);
+
+            if (result == DialogResult.Yes)
+            {
+                string mensagemSim = "Você votou SIM";
+                string titleSim = "Obrigado";
+                MessageBox.Show(mensagemSim, titleSim);
+                contadorSim++;
+                label1.Text = contadorSim.ToString();
+            }
+          
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mensagem = "Você deseja confirmar seu voto?";
+            string title = "Confirmação";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(mensagem, title, buttons);
+
+            if (result == DialogResult.Yes)
+            {
+                string mensagemNao = "Você votou NÃO";
+                string titleNao = "Que pena";
+                MessageBox.Show(mensagemNao, titleNao);
+               contadorNao++;
+               label3.Text = contadorNao.ToString();
+            }
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
